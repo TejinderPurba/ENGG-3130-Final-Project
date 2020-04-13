f4db9e4b16ff65da31544237ec47b7179733c606
TejinderPurba <tpurba@uoguelph.ca>
2020-04-13 16:30:22 -0400

    Added TSP & VRP capacity constraints

--- Final_Project.py
+++ Final_Project.py
@@ -8,6 +8,7 @@ import numpy as np
 import json
 import string
 import pickle
+import random
 import matplotlib.pyplot as plt
 from itertools import combinations 
 
@@ -28,11 +29,6 @@ from ortools.constraint_solver import pywrapcp
 data_file = "./data/1_02.xlsm" #Enter the data file for the day to schedule
 driver = '1642'
 sleeman_location = "551 Clair Rd W, Guelph, ON N1L 1E9"
-    
-wb = load_workbook(data_file, read_only=False, keep_vba=True)
-ws = wb.active
-deliveries = {}
-load_weights = {}
 
 options = RouteOptions()
 service = RouteService('cJrHDlXqG2L8mmWofS5FpftdRGDbDj00')
@@ -80,6 +76,11 @@ def calc_edges(locations, loads=None, factor=None, factor_weight=None):
     return routeMatrix
   
 def create_driver_graph():
+
+    wb = load_workbook(data_file, read_only=False, keep_vba=True)
+    ws = wb.active
+    deliveries = {}
+    load_weights = {}
 
     # Parse the excel sheet data
     for row in range(1, len(ws['A'])+1):
@@ -175,6 +176,22 @@ def create_data_model(num_vehicles=1):
 
     return data
 
+"""https://developers.google.com/optimization/routing/tsp"""
+def print_solution_tsp(manager, routing, solution):
+    """Prints solution on console."""
+    print('Objective: {} miles'.format(solution.ObjectiveValue()))
+    index = routing.Start(0)
+    plan_output = 'Route for vehicle 0:\n'
+    route_distance = 0
+    while not routing.IsEnd(index):
+        plan_output += ' {} ->'.format(manager.IndexToNode(index))
+        previous_index = index
+        index = solution.Value(routing.NextVar(index))
+        route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
+    plan_output += ' {}\n'.format(manager.IndexToNode(index))
+    print(plan_output)
+    plan_output += 'Route distance: {}miles\n'.format(route_distance)
+
 """https://developers.google.com/optimization/routing/vrp"""
 def print_solution_vrp(data, manager, routing, solution):
     """Prints solution on console."""
@@ -195,21 +212,33 @@ def print_solution_vrp(data, manager, routing, solution):
         max_route_distance = max(route_distance, max_route_distance)
     print('Maximum of the route distances: {}m'.format(max_route_distance))
 
-"""https://developers.google.com/optimization/routing/tsp"""
-def print_solution_tsp(manager, routing, solution):
+def print_solution_vrp_limited(data, manager, routing, solution):
     """Prints solution on console."""
-    print('Objective: {} miles'.format(solution.ObjectiveValue()))
-    index = routing.Start(0)
-    plan_output = 'Route for vehicle 0:\n'
-    route_distance = 0
-    while not routing.IsEnd(index):
-        plan_output += ' {} ->'.format(manager.IndexToNode(index))
-        previous_index = index
-        index = solution.Value(routing.NextVar(index))
-        route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
-    plan_output += ' {}\n'.format(manager.IndexToNode(index))
-    print(plan_output)
-    plan_output += 'Route distance: {}miles\n'.format(route_distance)
+
+    total_distance = 0
+    total_load = 0
+    for vehicle_id in range(data['num_vehicles']):
+        index = routing.Start(vehicle_id)
+        plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+        route_distance = 0
+        route_load = 0
+        while not routing.IsEnd(index):
+            node_index = manager.IndexToNode(index)
+            route_load += data['demands'][node_index]
+            plan_output += ' {0} Load({1}) -> '.format(node_index, route_load)
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(
+                previous_index, index, vehicle_id)
+        plan_output += ' {0} Load({1})\n'.format(manager.IndexToNode(index),
+                                                 route_load)
+        plan_output += 'Distance of the route: {}m\n'.format(route_distance)
+        plan_output += 'Load of the route: {}\n'.format(route_load)
+        print(plan_output)
+        total_distance += route_distance
+        total_load += route_load
+    print('Total distance of all routes: {}m'.format(total_distance))
+    print('Total load of all routes: {}'.format(total_load))
 
 """https://developers.google.com/optimization/routing/tsp"""
 def tsp_processing(data=None):
@@ -236,7 +265,7 @@ def tsp_processing(data=None):
 
     # Setting first solution heuristic.
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    #search_parameters.local_search_metaheuristic = (routing_enums_pb2.LocalSearchMetaheuristic.GREEDY_DESCENT)
+    search_parameters.local_search_metaheuristic = (routing_enums_pb2.LocalSearchMetaheuristic.GREEDY_DESCENT)
     search_parameters.time_limit.seconds = 30
     #search_parameters.log_search = True
     search_parameters.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC) # FIRST SOLUTION STRATEGY WILL BE USED TO COMPARE ALGORITHMS
@@ -286,7 +315,7 @@ def vrp_processing(data=None):
     # Setting first solution heuristic.
 
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    #search_parameters.local_search_metaheuristic = (routing_enums_pb2.LocalSearchMetaheuristic.GREEDY_DESCENT)
+    search_parameters.local_search_metaheuristic = (routing_enums_pb2.LocalSearchMetaheuristic.GREEDY_DESCENT)
     search_parameters.time_limit.seconds = 30
     #search_parameters.log_search = True
     search_parameters.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC) # FIRST SOLUTION STRATEGY WILL BE USED TO COMPARE ALGORITHMS
@@ -298,7 +327,59 @@ def vrp_processing(data=None):
     if solution:
         print_solution_vrp(data, manager, routing, solution)
 
-def loadRouteMatrix(fileName=None):
+"""https://developers.google.com/optimization/routing/cvrp"""
+def vrp_limited_processing(data=None):
+
+    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']), data['num_vehicles'], data['depot'])
+
+    # Create Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+
+
+    # Create and register a transit callback.
+    def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes."""
+        # Convert from routing variable Index to distance matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data['distance_matrix'][from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+
+    # Add Capacity constraint.
+    def demand_callback(from_index):
+        """Returns the demand of the node."""
+        # Convert from routing variable Index to demands NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        return data['demands'][from_node]
+
+    demand_callback_index = routing.RegisterUnaryTransitCallback(demand_callback)
+    routing.AddDimensionWithVehicleCapacity(
+        demand_callback_index,
+        0,  # null capacity slack
+        data['vehicle_capacities'],  # vehicle maximum capacities
+        True,  # start cumul to zero
+        'Capacity')
+
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.local_search_metaheuristic = (routing_enums_pb2.LocalSearchMetaheuristic.GREEDY_DESCENT)
+    search_parameters.time_limit.seconds = 30
+    #search_parameters.log_search = True
+    search_parameters.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC) # FIRST SOLUTION STRATEGY WILL BE USED TO COMPARE ALGORITHMS
+
+    # Solve the problem.
+    solution = routing.SolveWithParameters(search_parameters)
+
+    # Print solution on console.
+    if solution:
+        print_solution_vrp_limited(data, manager, routing, solution)
+
+def loadPickle(fileName=None):
     with open(fileName, 'rb') as handle:
         data = pickle.load(handle)
 
@@ -306,29 +387,60 @@ def loadRouteMatrix(fileName=None):
 
 if __name__ == '__main__':
 
+    ###################################### TESTING AREA ######################################
 
     # Instantiate the data problem using the MapQuest library
     #data = create_data_model(num_vehicles=1)
 
-    # Instantiate the data problem using the Data File
-    #data = loadRouteMatrix(fileName='DistanceMatrix-1_02-1.pickle')
-    #data = loadRouteMatrix(fileName='DistanceMatrix-1_03-1.pickle')
-    #data = loadRouteMatrix(fileName='DistanceMatrix-1_04-1.pickle')
-    data = loadRouteMatrix(fileName='DistanceMatrix-1_05-1.pickle')
-    #data = loadRouteMatrix(fileName='DistanceMatrix-1_07-1.pickle')
+    # Instantiate the data problem using the Pickle files
+    data = loadPickle(fileName='DistanceMatrix-1_02-1.pickle')
+    #data = loadPickle(fileName='DistanceMatrix-1_03-1.pickle')
+    #data = loadPickle(fileName='DistanceMatrix-1_04-1.pickle')
+    #data = loadPickle(fileName='DistanceMatrix-1_05-1.pickle')
+    #data = loadPickle(fileName='DistanceMatrix-1_07-1.pickle')
 
-    # Perform TSP with 1 vehicle
+    # Perform TSP with 1 vehicle & no capacity limits
     data['num_vehicles'] = 1
-    print("TRAVELLING SALESMAN PROBLEM:\n")
+    print("\n----------------- TRAVELLING SALESMAN PROBLEM -----------------\n")
     tsp_processing(data=data)
 
-    # Perform VRP with 4 vehicles
+    # Perform VRP with 4 vehicles & no capacity limits
     data['num_vehicles'] = 4
-    print("VEHICLE ROUTING PROBLEM:\n")
+    print("\n----------------- VEHICLE ROUTING PROBLEM -----------------\n")
     vrp_processing(data=data)
+
+    # Perform VRP with 4 vehicles w/ limited capacity (Can change to 1 vehicle to do TSP w/ capacity)
+    data['num_vehicles'] = 4
+    data['demands'] = loadPickle(fileName='LoadMatrix-1_02-1.pickle') # Load weights for the deliveries
+    data['vehicle_capacities'] = [3000, 3000, 3000, 3000]
+    print("\n----------------- LIMITED CAPACITY VEHICLE ROUTING PROBLEM ----------------- \n")
+    vrp_limited_processing(data=data)
 
     """Show complete delivery destination graph"""
     #graph_and_edges = create_driver_graph()
     #routeGraph = graph_and_edges[0]
     #edges = graph_and_edges[1]
     #display_graph(routeGraph, edges)  
+
+    ###################################### EXPERIMENTS LIST ######################################
+
+    # ------------ BASELINE ALGORITHM ------------
+    # 1.) Randomly Chosen [All Pickles]
+
+
+    # ------------ PATH_CHEAPEST_ARC ALGORITHM w/ GREEDY DESCENT LOCAL SEARCH ------------
+    # 1.) TSP(Unlimited Capacity) VS. VRP(Unlimited Capacity) [All Pickles]
+
+
+    # 2.) TSP(Unlimited Capacity) VS. VRP(Limited Capacity) [All Pickles]
+
+
+    # 3.) TSP(Limited Capacity) VS. VRP (Limited Capacity) [All Pickles]
+
+
+
+    # ------------ SAVINGS ALGORITHM w/ GREEDY DESCENT LOCAL SEARCH ------------
+    # 1.) TSP(Unlimited Capacity) VS. VRP(Unlimited Capacity) [All Pickles]
+    # 2.) TSP(Unlimited Capacity( VS. VRP(Limited Capacity) [All Pickles]
+    # 3.) TSP(Limited Capacity) VS. VRP (Limited Capacity) [All Pickles]
+
